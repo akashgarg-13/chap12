98393e90bb58401ab2029e87832afc8287c19266
AKASH GARG <you@example.com>
2021-12-23 18:42:21 +0530

    command line programs

---
+++ greplib/target/debug/deps/greplib-e156ce85387a2596.d
@@ -0,0 +1,5 @@
+/home/akash/rust/grep2/greplib/target/debug/deps/greplib-e156ce85387a2596.rmeta: src/lib.rs
+
+/home/akash/rust/grep2/greplib/target/debug/deps/greplib-e156ce85387a2596.d: src/lib.rs
+
+src/lib.rs:
---
+++ greplib/target/debug/deps/greplib-f208099bb41d10ca.d
@@ -0,0 +1,5 @@
+/home/akash/rust/grep2/greplib/target/debug/deps/greplib-f208099bb41d10ca.rmeta: src/lib.rs
+
+/home/akash/rust/grep2/greplib/target/debug/deps/greplib-f208099bb41d10ca.d: src/lib.rs
+
+src/lib.rs:
